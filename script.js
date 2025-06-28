640c9cb106dd5e4e9e3f3f93005afcfab3c24620
Abhishekfc <abhisheksir6280@gmail.com>
2025-06-28 22:46:39 +0530

    created library

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 console.log("let play with javascript")
+
+async function getSongs() {
+
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+
+
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+
+
+    let songs = [];
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1])
+        }
+    }
+    return songs;
+}
+
+async function main() {
+    //get the list of all songs
+    let songs = await getSongs();
+
+
+
+    //show all the songs in playslist
+    let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUl.innerHTML = songUl.innerHTML + ` <li>
+                                <img class="invert "src="music.svg" alt="">
+                                <div class="info">
+                                    <div>${song.replaceAll("%20", " ")}</div>
+                                    <div>Abhishek Sharma</div>
+                                </div>
+                                <div class="playnow">
+                                    <span>Play Now</span>
+                                    <img class="invert" src="play.svg" alt="">
+                                </div></li>`;
+    }
+    var audio = new Audio(songs[2]);
+    audio.play();
+
+
+}
+
+main();
